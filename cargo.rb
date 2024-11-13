8cb88f65f4a18ed482c74d1ba21d33acdf9615ae
Keenan Bromley <bromleyk@oregonstate.edu>
2024-11-12 16:04:45 -0800

    Created a new cargo class

---
+++ cargo.rb
@@ -0,0 +1,30 @@
+# Cargo
+
+#New class that only focuses on cargo
+class Cargo
+
+  MAX_CARGO_ITEMS = 10
+  
+  attr_accessor :cargo_contents
+    
+  def initialize()
+      @cargo_contents = []
+  end
+  
+  def add_cargo(item)
+    self.cargo_contents << item
+  end
+
+  def remove_cargo(item)
+    self.cargo_contents.remove(item)
+  end
+
+  def pannier_capacity
+    MAX_CARGO_ITEMS
+  end
+
+  def pannier_remaining_capacity
+    MAX_CARGO_ITEMS - self.cargo_contents.size
+  end
+
+end
