62b2f364e514cb7bc53c817a4e920c4fd534dc1c
Keenan Bromley <bromleyk@oregonstate.edu>
2024-11-12 16:06:44 -0800

    Updated classes with new cargo class

--- bike.rb
+++ bike.rb
@@ -19,21 +19,6 @@ class Bike
   def rent!
     self.rented = true
   end
-
-  def add_cargo(item)
-    self.cargo_contents << item
-  end
-
-  def remove_cargo(item)
-    self.cargo_contents.remove(item)
-  end
-
-  def pannier_capacity
-    MAX_CARGO_ITEMS
-  end
-
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
-  end
-
 end
+
+#Got rid of the cargo portion and turned it into its own class
--- main.rb
+++ main.rb
@@ -1,13 +1,16 @@
 !# /usr/bin/env ruby
 
 require_relative 'bike'
+require_relative 'cargo'
 
 bike = Bike.new(1, :pink, 99.99)
+#Added cargo object for the new cargo class
+cargo = Cargo.new()
 
-bike.add_cargo(:apples)
-bike.add_cargo(:water)
-bike.add_cargo(:repair_kit)
+cargo.add_cargo(:apples)
+cargo.add_cargo(:water)
+cargo.add_cargo(:repair_kit)
 
-puts "Space for #{bike.pannier_remaining_capacity} items left."
+puts "Space for #{cargo.pannier_remaining_capacity} items left."
 
 bike.rent!
