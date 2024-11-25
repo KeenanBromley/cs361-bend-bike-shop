da7c94d87882eb5e0cbfd1ac8d70b783d39bc527
Keenan Bromley <bromleyk@oregonstate.edu>
2024-11-24 19:42:51 -0800

    Created changes based on canvas requirements

--- bike.rb
+++ bike.rb
@@ -5,14 +5,13 @@ class Bike
 
   STANDARD_WEIGHT = 200 # lbs
 
-  attr_reader :id, :color, :price, :weight, :luggage
-
-  def initialize(id, color, price, extra_items)
+  attr_reader :id, :color, :price, :weight
+  
+  def initialize(id, color, price)
     @id = id
     @color = color
     @price = price
     @weight = STANDARD_WEIGHT
-    @luggage = Luggage.new(Luggage::DEFAULT_MAX_CAPACITY, extra_items, self)
   end
 
 end
